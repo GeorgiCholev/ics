0890d2bc5f1952ce236c492362e59bd4a358ec4b
GeorgiCholev <georgi.k.cholev@gmail.com>
2023-05-03 11:20:54 +0300

    Response statuses

--- src/main/java/com/example/ics/controllers/ImageController.java
+++ src/main/java/com/example/ics/controllers/ImageController.java
@@ -3,6 +3,8 @@ package com.example.ics.controllers;
 import com.example.ics.models.dtos.ImageAddressDto;
 import com.example.ics.models.dtos.TagsContainerRespDto;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,14 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImageController {
 
     @PostMapping
-    public TagsContainerRespDto postImageAddress(@Valid @RequestBody final ImageAddressDto addressDto,
-                                                 BindingResult bindingResult) {
+    public ResponseEntity<TagsContainerRespDto> postImageAddress
+            (@Valid @RequestBody final ImageAddressDto addressDto, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            System.out.println("Invalid address");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        System.out.println("Valid Address");
-        return new TagsContainerRespDto();
+        return new ResponseEntity<>(new TagsContainerRespDto(), HttpStatus.OK);
     }
 }
