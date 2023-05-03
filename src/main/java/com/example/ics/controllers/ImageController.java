6471f405d20f4cd8d91beefc0a32bd4b6dddb61a
GeorgiCholev <georgi.k.cholev@gmail.com>
2023-05-03 16:05:45 +0300

    Handle API call to Imagga

--- src/main/java/com/example/ics/controllers/ImageController.java
+++ src/main/java/com/example/ics/controllers/ImageController.java
@@ -2,6 +2,7 @@ package com.example.ics.controllers;
 
 import com.example.ics.models.dtos.ImageAddressDto;
 import com.example.ics.models.dtos.TagsContainerRespDto;
+import com.example.ics.services.TagGenerationService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/images")
 public class ImageController {
 
+    private final TagGenerationService tagGenerationService;
+
+    public ImageController(TagGenerationService tagGenerationService) {
+        this.tagGenerationService = tagGenerationService;
+    }
+
     @PostMapping
     public ResponseEntity<TagsContainerRespDto> postImageAddress
             (@Valid @RequestBody final ImageAddressDto addressDto, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        TagsContainerRespDto tagsDto = tagGenerationService.generateTags(addressDto);
+        if (tagsDto == null) {
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
         }
 
         return new ResponseEntity<>(new TagsContainerRespDto(), HttpStatus.OK);
