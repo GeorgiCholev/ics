1400ba908934620d0c5c82b081dca9ef09be3480
GeorgiCholev <georgi.k.cholev@gmail.com>
2023-06-05 09:48:25 +0300

    Fix gallery component in angular, make it faster in scrolling

--- ics-client/src/app/image-gallery/image-gallery.component.ts
+++ ics-client/src/app/image-gallery/image-gallery.component.ts
@@ -18,7 +18,7 @@ export class ImageGalleryComponent implements OnInit, OnDestroy {
     images: BehaviorSubject<Image[]> = new BehaviorSubject<Image[]>([]);
     finished = false;
     pageNum = 0;
-    
+
     currentlyFetching: boolean = false;
 
     constructor(private dataAccessService: DataAccessService, private navigationService: NavigationService,
@@ -28,18 +28,95 @@ export class ImageGalleryComponent implements OnInit, OnDestroy {
         this.titleService.setTitle(this.componentTitle);
     }
 
-    ngOnInit(): void {
-        this.activatedRoute.queryParams.subscribe(params => {
-            this.imageHandleService.setCurrentQueryParams(params);
-            this.currentlyFetching = true;
-            this.getImages(false);
-        });
+    ngOnInit() {
+        let queryParams = this.activatedRoute.snapshot.queryParams;
+        this.imageHandleService.setCurrentQueryParams(queryParams);
+        this.getImages();
     }
 
     onScroll() {
+        let timeout = 0;
+        if (this.currentlyFetching) {
+            timeout = 2_000;
+        }
+
         setTimeout(() => {
-            this.getImages(false);
-        }, 4_000);
+            this.getImages();
+        }, timeout);
+    }
+
+    getImages() {
+        if (this.finished) {
+            return;
+        }
+
+        this.currentlyFetching = true;
+
+        let currentPage = this.pageNum;
+        this.pageNum++;
+
+        let newImages = this.imageHandleService.getImagesPageFromIndex(currentPage);
+        if (newImages) {
+            this.addImagesToBehaviourSubject(this.images, newImages);
+            if (!this.bottomIsReached(newImages)) {
+                this.getNextPageInAdvance();
+                return;
+            }
+            this.currentlyFetching = false;
+            this.finished = true;
+        } else {
+            this.dataAccessService
+                .getImagesPageOf(this.imageHandleService.galleryQueryParams, currentPage)
+                .subscribe({
+                    next: (dataArr) => {
+                        if (dataArr) {
+                            this.addImagesToBehaviourSubject(this.images, dataArr);
+                            this.addToIndices(dataArr, currentPage);
+
+                            if (!this.bottomIsReached(dataArr)) {
+                                this.getNextPageInAdvance();
+                                return;
+                            }
+                        }
+                        this.currentlyFetching = false;
+                        this.finished = true;
+                    }
+                });
+        }
+    }
+
+    getNextPageInAdvance() {
+        let newImages = this.imageHandleService.getImagesPageFromIndex(this.pageNum);
+
+        if (this.finished || newImages) {
+            this.currentlyFetching = false;
+            return;
+        }
+
+        this.dataAccessService
+            .getImagesPageOf(this.imageHandleService.galleryQueryParams, this.pageNum)
+            .subscribe({
+                next: (dataArr) => {
+                    if (dataArr) {
+                        this.addToIndices(dataArr, this.pageNum);
+                        this.currentlyFetching = false;
+                    }
+                }
+            });
+    }
+
+    private addToIndices(dataArr: Image[], pageNum: number) {
+        dataArr.forEach(data => this.imageHandleService.addImageByIdToIndex(data));
+        this.imageHandleService.addImagesPageToIndex(pageNum, dataArr);
+    }
+
+    private bottomIsReached(dataArr: Image[]): boolean {
+        return dataArr.length < this.imageHandleService.batchSize;
+    }
+
+
+    private addImagesToBehaviourSubject(behaviourSubject: BehaviorSubject<Image[]>, dataArr: Image[]) {
+        behaviourSubject.next(lodash.concat(behaviourSubject.getValue(), dataArr));
     }
 
     onImgClick(imageId: string) {
@@ -47,78 +124,17 @@ export class ImageGalleryComponent implements OnInit, OnDestroy {
             .catch(err => console.log(err));
     }
 
+
     getTopThreeTags(image: Image) {
         const tagEntries: [string, number][] = Object.entries(image.tags);
         tagEntries.sort((a, b) => b[1] - a[1]);
         return tagEntries.slice(0, 3);
     }
 
-
     ngOnDestroy(): void {
         this.images.unsubscribe();
     }
 
-    private getImages(onlyAddToIndices: boolean) {
-
-        if (this.finished) {
-            this.currentlyFetching = false;
-            return;
-        }
-
-        let newImages = this.imageHandleService.getImagesPageFromIndex(this.pageNum);
-        if (newImages) {
-            this.addImagesToBehaviourSubject(this.images, newImages);
-            this.determineIfScrollingEnds(newImages);
-            this.pageNum++;
-
-            this.getNextPageInAdvance(!onlyAddToIndices);
-        } else {
-
-            this.dataAccessService
-                .getImagesPageOf(this.imageHandleService.galleryQueryParams, this.pageNum)
-                .subscribe({
-                    next: (dataArr) => {
-                        if (dataArr) {
-                            let currentPage = this.pageNum;
-                            if (!onlyAddToIndices) {
-                                this.addImagesToBehaviourSubject(this.images, dataArr);
-                                this.determineIfScrollingEnds(dataArr);
-                                this.pageNum++;
-                            }
-                            this.addToIndices(dataArr, currentPage);
-                        } else {
-                            this.finished = true;
-                        }
-
-                        this.getNextPageInAdvance(!onlyAddToIndices);
-                    }
-                });
-        }
-    }
-
-    private getNextPageInAdvance(isEnabled: boolean) {
-        if (isEnabled) {
-            this.getImages(true);
-        } else {
-            this.currentlyFetching = false;
-        }
-    }
-
-
-    private addToIndices(dataArr: Image[], pageNum: number) {
-        dataArr.forEach(data => this.imageHandleService.addImageByIdToIndex(data));
-        this.imageHandleService.addImagesPageToIndex(pageNum, dataArr);
-    }
-
-    private determineIfScrollingEnds(dataArr: Image[]) {
-        if (dataArr.length < this.imageHandleService.batchSize) {
-            this.finished = true;
-        }
-    }
-
-    private addImagesToBehaviourSubject(behaviourSubject: BehaviorSubject<Image[]>, dataArr: Image[]) {
-        behaviourSubject.next(lodash.concat(behaviourSubject.getValue(), dataArr));
-    }
-
     protected readonly Object = Object;
+    
 }
